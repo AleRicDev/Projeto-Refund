601e51d179ea35309c6eec91031d39aa5ac4ee09
Alessandro Ricardo <Alessandroricardo337@gmail.com>
2026-02-20 16:04:51 -0300

    add category icon to expense item

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 //Seleciona os elementos do formulário.
+const form = document.querySelector("form")
 const amount =  document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
+//Captura o evento de input para formatar o valor
 amount.oninput = () => {
     //Obtém o valor atual do input e remove os caracteres não numéricos.
     let value = amount.value.replace(/\D/g, "")
@@ -25,3 +32,48 @@ function formatCurrencyBRL(value){
 
     return value
 }
+// Captura o evento de submit do Formulário para obter os valores.
+form.onsubmit = (event) => {
+    //Previne o comportamento padrão de recarregar a página.
+    event.preventDefault()
+
+    //Cria um objeto com os detalhes na nova despesa.
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    //Chama a função que irá adicionar o item na lista.
+   expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+
+    try {
+        //Cria o alimento o elemento para Adicionar na lista.
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+       
+        // Cria o ícone da categoria
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        //adiciona as informações no item.
+        expenseItem.append(expenseIcon)
+
+        //Adiciona o item na lista
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas")
+        console.log(error)
+    }
+    
+}
+
+
